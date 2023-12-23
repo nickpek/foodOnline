7aa36bbdcd07205a0dde636ee7c326bf5c1524e9
Nikolaos Pekridis <nick.pekridis@t-ris.co.uk>
2023-12-23 19:03:05 +0000

    food CRUD operation

--- accounts/validators.py
+++ accounts/validators.py
@@ -4,6 +4,6 @@ import os
 def allow_only_images_validator(value):
     ext = os.path.splitext(value.name)[1] # cover-image.jpg so ext=jpg
     print (ext)
-    valid_extensions = ['.png', 'jpg', 'jpeg']
+    valid_extensions = ['.png', '.jpg', '.jpeg']
     if not ext.lower() in valid_extensions:
         raise ValidationError('Unsupported file extension. Allowed extensions: ' + str(valid_extensions))
